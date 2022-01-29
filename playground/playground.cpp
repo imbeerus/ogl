1441b0e33499312b7977e64e7dd70a4774e08fb0
Zinovyev Ivan <izinovyev42@gmail.com>
2022-01-29 17:03:39 +0300

    add triangle draw

--- playground/playground.cpp
+++ playground/playground.cpp
@@ -54,6 +54,12 @@ static const GLfloat g_cube_vertex_buffer_data[] = {
 	1.0f,-1.0f, 1.0f
 };
 
+static const GLfloat g_triangle_vertex_buffer_data[] = {
+	-3.0f, -2.0f, -2.0f,
+	-2.0f, -2.0f,-3.0f,
+	-1.0f, 0.0f,-1.0f
+};
+
 // One color for each vertex. 
 // Our vertices. Three consecutive floats give a 3D vertex; Three consecutive vertices give a triangle.
 // A cube has 6 faces with 2 triangles each, so this makes 6*2=12 triangles, and 12*3 vertices
@@ -70,9 +76,17 @@ GLuint CubeMatrixID;
 GLuint cubeVertexbuffer;
 glm::mat4 CubeMVP;
 
+GLuint TriangleMatrixID;
+GLuint triangleVertexbuffer;
+glm::mat4 TriangleMVP;
+
 GLuint colorbuffer;
 
 GLFWwindow* window;
+
+bool handleEscapeKey() {
+	return glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS && glfwWindowShouldClose(window) == 0;
+}
 
 void setupGLFW() {
 	glfwWindowHint(GLFW_SAMPLES, 4);
@@ -104,12 +118,16 @@ void bindColorBuffer() {
 void initCubeMVP() {
 	// Get a handle for our "MVP" uniform
 	CubeMatrixID = glGetUniformLocation(shaderProgramID, "MVP");
-
 	// Model matrix : an identity matrix (model will be at the origin)
 	glm::mat4 CubeModel = glm::mat4(1.0f);
-
 	// Our ModelViewProjection : multiplication of our 3 matrices
 	CubeMVP = Projection * View * CubeModel; // Remember, matrix multiplication is the other way around
+}
+
+void initTriangleMVP() {
+	TriangleMatrixID = glGetUniformLocation(shaderProgramID, "MVP");
+	glm::mat4 TriangleModel = glm::mat4(1.0f);
+	TriangleMVP = Projection * View * TriangleModel;
 }
 
 void initCubeBuffer() {
@@ -118,8 +136,63 @@ void initCubeBuffer() {
 	glBufferData(GL_ARRAY_BUFFER, sizeof(g_cube_vertex_buffer_data), g_cube_vertex_buffer_data, GL_STATIC_DRAW);
 }
 
-bool handleEscapeKey() {
-	return glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS && glfwWindowShouldClose(window) == 0;
+void initTriangleBuffer() {
+	glGenBuffers(1, &triangleVertexbuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, triangleVertexbuffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(g_triangle_vertex_buffer_data), g_triangle_vertex_buffer_data, GL_STATIC_DRAW);
+}
+
+void drawCube() {
+	// 1rst attribute buffer : vertices
+	glBindBuffer(GL_ARRAY_BUFFER, cubeVertexbuffer);
+	glVertexAttribPointer(
+		0,                  // attribute. No particular reason for 0, but must match the layout in the shader.
+		3,                  // size
+		GL_FLOAT,           // type
+		GL_FALSE,           // normalized?
+		0,                  // stride
+		(void*)0            // array buffer offset
+	);
+
+	// 2nd attribute buffer : colors
+	glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(g_cube_vertex_buffer_data), g_color_buffer_data, GL_STATIC_DRAW);
+	glVertexAttribPointer(
+		1,                                // attribute. No particular reason for 1, but must match the layout in the shader.
+		3,                                // size
+		GL_FLOAT,                         // type
+		GL_FALSE,                         // normalized?
+		0,                                // stride
+		(void*)0                          // array buffer offset
+	);
+
+	// Draw the cube !
+	glDrawArrays(GL_TRIANGLES, 0, 12 * 3); // 12*3 indices starting at 0 -> 12 triangles -> 6 squares
+}
+
+void drawTriangle() {
+	glBindBuffer(GL_ARRAY_BUFFER, triangleVertexbuffer);
+	glVertexAttribPointer(
+		0,                  
+		3,                
+		GL_FLOAT,           
+		GL_FALSE,          
+		0,                  
+		(void*)0            
+	);
+
+	glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(g_triangle_vertex_buffer_data), g_color_buffer_data, GL_STATIC_DRAW);
+	glVertexAttribPointer(
+		1,                                
+		3,                               
+		GL_FLOAT,                         
+		GL_FALSE,                         
+		0,                                
+		(void*)0                          
+	);
+
+	glDrawArrays(GL_TRIANGLES, 0, 3);
 }
 
 int main(void)
@@ -156,6 +229,9 @@ int main(void)
 	// Ensure we can capture the escape key being pressed below
 	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
 
+    // Accept fragment if it closer to the camera than the former one
+	glDepthFunc(GL_LESS);
+
 	// Dark background
 	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
 
@@ -171,13 +247,16 @@ int main(void)
 
 	// Camera matrix
     View = glm::lookAt(
-		glm::vec3(4, -3, 3), // Camera is at (4,-3,3), in World Space
+		glm::vec3(10, -10, 10), // Camera is at (4,-3,3), in World Space
 		glm::vec3(0, 0, 0), // and looks at the origin
 		glm::vec3(0, 1, 0)  // Head is up (set to 0,-1,0 to look upside-down)
 	);
 
 	initCubeMVP();
 	initCubeBuffer();
+
+	initTriangleMVP();
+	initTriangleBuffer();
 
 	updateColors();
 	bindColorBuffer();
@@ -193,36 +272,15 @@ int main(void)
 		// Send our transformation to the currently bound shader, 
 		// in the "MVP" uniform
 		glUniformMatrix4fv(CubeMatrixID, 1, GL_FALSE, &CubeMVP[0][0]);
+		glUniformMatrix4fv(TriangleMatrixID, 1, GL_FALSE, &TriangleMVP[0][0]);
 
 		glEnableVertexAttribArray(0);
 		glEnableVertexAttribArray(1);
 
-		// 1rst attribute buffer : vertices
-		glBindBuffer(GL_ARRAY_BUFFER, cubeVertexbuffer);
-		glVertexAttribPointer(
-			0,                  // attribute. No particular reason for 0, but must match the layout in the shader.
-			3,                  // size
-			GL_FLOAT,           // type
-			GL_FALSE,           // normalized?
-			0,                  // stride
-			(void*)0            // array buffer offset
-		);
-
-		// 2nd attribute buffer : colors
 		updateColors();
-		glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
-		glBufferData(GL_ARRAY_BUFFER, sizeof(g_color_buffer_data), g_color_buffer_data, GL_STATIC_DRAW);
-		glVertexAttribPointer(
-			1,                                // attribute. No particular reason for 1, but must match the layout in the shader.
-			3,                                // size
-			GL_FLOAT,                         // type
-			GL_FALSE,                         // normalized?
-			0,                                // stride
-			(void*)0                          // array buffer offset
-		);
 
-		// Draw the triangle !
-		glDrawArrays(GL_TRIANGLES, 0, 12 * 3); // 12*3 indices starting at 0 -> 12 triangles -> 6 squares
+		drawCube();
+		drawTriangle();
 
 		glDisableVertexAttribArray(0);
 		glDisableVertexAttribArray(1);
@@ -236,6 +294,7 @@ int main(void)
 
 	// Cleanup VBO and shader
 	glDeleteBuffers(1, &cubeVertexbuffer);
+	glDeleteBuffers(1, &triangleVertexbuffer);
 	glDeleteBuffers(1, &colorbuffer);
 	glDeleteProgram(shaderProgramID);
 	glDeleteVertexArrays(1, &VertexArrayID);
