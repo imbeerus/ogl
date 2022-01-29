ee2d2f5890e60726eb3d06a523705f1ffd36bb16
Zinovyev Ivan <izinovyev42@gmail.com>
2022-01-29 16:00:11 +0300

    refactoring

--- playground/playground.cpp
+++ playground/playground.cpp
@@ -1,19 +1,17 @@
 // Include standard headers
 #include <stdio.h>
 #include <stdlib.h>
-
 // Include GLEW
 #include <GL/glew.h>
-
 // Include GLFW
 #include <GLFW/glfw3.h>
-GLFWwindow* window;
-
 // Include GLM
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
-
 #include <common/shader.hpp>
+
+static const int WINDOW_WIDTH = 1280;
+static const int WINDOW_HEIGHT = 720;
 
 // Our vertices. Three consecutive floats give a 3D vertex; Three consecutive vertices give a triangle.
 // A cube has 6 faces with 2 triangles each, so this makes 6*2=12 triangles, and 12*3 vertices
@@ -61,13 +59,67 @@ static const GLfloat g_cube_vertex_buffer_data[] = {
 // A cube has 6 faces with 2 triangles each, so this makes 6*2=12 triangles, and 12*3 vertices
 static GLfloat g_color_buffer_data[12 * 3 * 3];
 
+GLuint VertexArrayID;
+
+glm::mat4 Projection;
+glm::mat4 View;
+
+GLuint shaderProgramID;
+
+GLuint CubeMatrixID;
+GLuint cubeVertexbuffer;
+glm::mat4 CubeMVP;
+
+GLuint colorbuffer;
+
+GLFWwindow* window;
+
+void setupGLFW() {
+	glfwWindowHint(GLFW_SAMPLES, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_FALSE);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); //We don't want the old OpenGL 
+}
+
 void updateColors() {
 	for (int v = 0; v < 12 * 3; v++) {
 		g_color_buffer_data[3 * v + 0] = (float(rand()) / float((RAND_MAX - v)));
 		g_color_buffer_data[3 * v + 1] = (float(rand()) / float((RAND_MAX - v)));
 		g_color_buffer_data[3 * v + 2] = (float(rand()) / float((RAND_MAX - v)));
 	}
+}
 
+void initVertexArray() {
+	glGenVertexArrays(1, &VertexArrayID);
+	glBindVertexArray(VertexArrayID);
+}
+
+void bindColorBuffer() {
+	glGenBuffers(1, &colorbuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(g_color_buffer_data), g_color_buffer_data, GL_STATIC_DRAW);
+}
+
+void initCubeMVP() {
+	// Get a handle for our "MVP" uniform
+	CubeMatrixID = glGetUniformLocation(shaderProgramID, "MVP");
+
+	// Model matrix : an identity matrix (model will be at the origin)
+	glm::mat4 CubeModel = glm::mat4(1.0f);
+
+	// Our ModelViewProjection : multiplication of our 3 matrices
+	CubeMVP = Projection * View * CubeModel; // Remember, matrix multiplication is the other way around
+}
+
+void initCubeBuffer() {
+	glGenBuffers(1, &cubeVertexbuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, cubeVertexbuffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(g_cube_vertex_buffer_data), g_cube_vertex_buffer_data, GL_STATIC_DRAW);
+}
+
+bool handleEscapeKey() {
+	return glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS && glfwWindowShouldClose(window) == 0;
 }
 
 int main(void)
@@ -80,17 +132,10 @@ int main(void)
 		return -1;
 	}
 
-	glfwWindowHint(GLFW_SAMPLES, 4);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_FALSE);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); //We don't want the old OpenGL 
-
-	int width = 1024;
-	int height = 768;
+	setupGLFW();
 
 	// Open a window and create its OpenGL context
-	window = glfwCreateWindow(width, height, "Playground", NULL, NULL);
+	window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Playground", NULL, NULL);
 	if (window == NULL) {
 		fprintf(stderr, "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible. Try the 2.1 version of the tutorials.\n");
 		getchar();
@@ -111,47 +156,31 @@ int main(void)
 	// Ensure we can capture the escape key being pressed below
 	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
 
-	// Dark blue background
+	// Dark background
 	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
 
-	GLuint VertexArrayID;
-	glGenVertexArrays(1, &VertexArrayID);
-	glBindVertexArray(VertexArrayID);
+	initVertexArray();
 
 	// Create and compile our GLSL program from the shaders
-	GLuint programID = LoadShaders("SimpleVertexShader.vertexshader", "SimpleFragmentShader.fragmentshader");
-
-	// Get a handle for our "MVP" uniform
-	GLuint MatrixID = glGetUniformLocation(programID, "MVP");
+	shaderProgramID = LoadShaders("SimpleVertexShader.vertexshader", "SimpleFragmentShader.fragmentshader");
 
 	// Projection matrix : 45° Field of View, 4:3 ratio, display range : 0.1 unit <-> 100 units
-	glm::mat4 Projection = glm::perspective(glm::radians(45.0f), (float) width / (float) height, 0.1f, 100.0f);
+	Projection = glm::perspective(glm::radians(45.0f), (float) WINDOW_WIDTH / (float) WINDOW_HEIGHT, 0.1f, 100.0f);
 	// Or, for an ortho camera :
 	// glm::mat4 Projection = glm::ortho(-10.0f,10.0f,-10.0f,10.0f,0.0f,100.0f); // In world coordinates
 
 	// Camera matrix
-	glm::mat4 View = glm::lookAt(
+    View = glm::lookAt(
 		glm::vec3(4, -3, 3), // Camera is at (4,-3,3), in World Space
 		glm::vec3(0, 0, 0), // and looks at the origin
 		glm::vec3(0, 1, 0)  // Head is up (set to 0,-1,0 to look upside-down)
 	);
-	// Model matrix : an identity matrix (model will be at the origin)
-	glm::mat4 CubeModel = glm::mat4(1.0f);
 
-	// Our ModelViewProjection : multiplication of our 3 matrices
-	glm::mat4 CUBE_MVP = Projection * View * CubeModel; // Remember, matrix multiplication is the other way around
-
-	GLuint vertexbuffer;
-	glGenBuffers(1, &vertexbuffer);
-	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(g_cube_vertex_buffer_data), g_cube_vertex_buffer_data, GL_STATIC_DRAW);
+	initCubeMVP();
+	initCubeBuffer();
 
 	updateColors();
-
-	GLuint colorbuffer;
-	glGenBuffers(1, &colorbuffer);
-	glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(g_color_buffer_data), g_color_buffer_data, GL_STATIC_DRAW);
+	bindColorBuffer();
 
 	do {
 		
@@ -159,15 +188,17 @@ int main(void)
 		glClear(GL_COLOR_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
 
 		// Use our shader
-		glUseProgram(programID);
+		glUseProgram(shaderProgramID);
 
 		// Send our transformation to the currently bound shader, 
 		// in the "MVP" uniform
-		glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &CUBE_MVP[0][0]);
+		glUniformMatrix4fv(CubeMatrixID, 1, GL_FALSE, &CubeMVP[0][0]);
+
+		glEnableVertexAttribArray(0);
+		glEnableVertexAttribArray(1);
 
 		// 1rst attribute buffer : vertices
-		glEnableVertexAttribArray(0);
-		glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+		glBindBuffer(GL_ARRAY_BUFFER, cubeVertexbuffer);
 		glVertexAttribPointer(
 			0,                  // attribute. No particular reason for 0, but must match the layout in the shader.
 			3,                  // size
@@ -179,7 +210,6 @@ int main(void)
 
 		// 2nd attribute buffer : colors
 		updateColors();
-		glEnableVertexAttribArray(1);
 		glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
 		glBufferData(GL_ARRAY_BUFFER, sizeof(g_color_buffer_data), g_color_buffer_data, GL_STATIC_DRAW);
 		glVertexAttribPointer(
@@ -201,14 +231,13 @@ int main(void)
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 
-	} // Check if the ESC key was pressed or the window was closed
-	while (glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS &&
-		glfwWindowShouldClose(window) == 0);
+	} 
+	while (handleEscapeKey());
 
 	// Cleanup VBO and shader
-	glDeleteBuffers(1, &vertexbuffer);
+	glDeleteBuffers(1, &cubeVertexbuffer);
 	glDeleteBuffers(1, &colorbuffer);
-	glDeleteProgram(programID);
+	glDeleteProgram(shaderProgramID);
 	glDeleteVertexArrays(1, &VertexArrayID);
 
 	// Close OpenGL window and terminate GLFW
